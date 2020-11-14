80ca05f4662982bb3851bc7a5cbd1a578e93acaf
Saxo_Broko <saxonbrooker@gmail.com>
2020-11-15 02:57:59 +1100

    added some missing files

---
+++ js/cse.js
@@ -0,0 +1,61 @@
+(function(opts_){window.__gcse=window.__gcse||{};window.__gcse.ct=(new Date).getTime();window.__gcse.scb=function(){var a=window.__gcse;a.plainStyle&&delete opts_.rawCss;google.search.cse.element.init(opts_)&&("explicit"!=a.parsetags?"complete"==document.readyState||"interactive"==document.readyState?(google.search.cse.element.go(),a.callback&&a.callback()):google.setOnLoadCallback(function(){google.search.cse.element.go();a.callback&&a.callback()},!0):a.callback&&a.callback())};
+var b=document.createElement("script"),c=opts_.protocol+"://"+opts_.uds+"/"+(opts_.loaderPath||"jsapi")+"?autoload=",d=encodeURIComponent,e='{"name":"search","version":"1.0","callback":"__gcse.scb"',f=window.__gcse;
+if(!f||!f.plainStyle){var g=opts_.protocol+(opts_.uiOptions&&opts_.uiOptions.enableMobileLayout?"://www.google.com/cse/static/style/look/mobile/":"://www.google.com/cse/static/style/look/"),h,k=opts_.theme,l=opts_.uiOptions.forceV2LookAndFeel;k=k.toLowerCase();h=l?"v2/"+k.replace("v2_",""):k.replace("v2_","v2/");e+=',"style":"'+(g+h+".css")+'"'}opts_.language&&(e+=',"language":"'+opts_.language+'"');var m=c+d('{"modules":['+(e+"}")+"]}");opts_.callback&&(m=m+"&callback="+opts_.callback);b.src=m+"";
+b.type="text/javascript";document.getElementsByTagName("head")[0].appendChild(b);
+})({
+  "cx": "009867514131680730295:fqjmyncyg1s",
+  "language": "en",
+  "theme": "MINIMALIST",
+  "uiOptions": {
+    "resultsUrl": "https://web.archive.org/web/20190618003258/https://home.saxobroko.ga/search",
+    "enableAutoComplete": true,
+    "autoCompleteMatchType": "any",
+    "enableImageSearch": true,
+    "imageSearchLayout": "popup",
+    "resultSetSize": "filtered_cse",
+    "enableOrderBy": true,
+    "orderByOptions": [{
+      "label": "Relevance",
+      "key": ""
+    }, {
+      "label": "Date",
+      "key": "date"
+    }],
+    "overlayResults": false,
+    "queryParameterName": "q",
+    "enableMobileLayout": false,
+    "numTopRefinements": -1,
+    "isSitesearch": true,
+    "enableSpeech": true,
+    "forceV2LookAndFeel": true,
+    "hideElementBranding": false
+  },
+  "protocol": "https",
+  "uds": "www.google.com",
+  "rawCss": ".gsc-control-cse{font-family:Verdana, sans-serif;border-color:#000000;background-color:#444444}.gsc-control-cse .gsc-table-result{font-family:Verdana, sans-serif}input.gsc-input,.gsc-input-box,.gsc-input-box-hover,.gsc-input-box-focus{border-color:#000000}.gsc-search-button-v2,.gsc-search-button-v2:hover,.gsc-search-button-v2:focus{border-color:#000000;background-color:#333333;background-image:none;filter:none}.gsc-search-button-v2 svg{fill:#FFFFFF}.gsc-tabHeader.gsc-tabhInactive{border-color:#000000;background-color:#000000}.gsc-tabHeader.gsc-tabhActive{border-color:#000000;border-bottom-color:#000000;background-color:#000000}.gsc-tabsArea{border-color:#000000}.gsc-webResult.gsc-result,.gsc-results .gsc-imageResult{border-color:#000000;background-color:#444444}.gsc-webResult.gsc-result:hover,.gsc-imageResult:hover{border-color:#000000;background-color:#333333}.gs-webResult.gs-result a.gs-title:link,.gs-webResult.gs-result a.gs-title:link b,.gs-imageResult a.gs-title:link,.gs-imageResult a.gs-title:link b{color:#999999}.gs-webResult.gs-result a.gs-title:visited,.gs-webResult.gs-result a.gs-title:visited b,.gs-imageResult a.gs-title:visited,.gs-imageResult a.gs-title:visited b{color:#000000}.gs-webResult.gs-result a.gs-title:hover,.gs-webResult.gs-result a.gs-title:hover b,.gs-imageResult a.gs-title:hover,.gs-imageResult a.gs-title:hover b{color:#666666}.gs-webResult.gs-result a.gs-title:active,.gs-webResult.gs-result a.gs-title:active b,.gs-imageResult a.gs-title:active,.gs-imageResult a.gs-title:active b{color:#000000}.gsc-cursor-page{color:#999999}a.gsc-trailing-more-results:link{color:#999999}.gs-webResult .gs-snippet,.gs-imageResult .gs-snippet,.gs-fileFormatType{color:#CCCCCC}.gs-webResult div.gs-visibleUrl,.gs-imageResult div.gs-visibleUrl{color:#000000}.gs-webResult div.gs-visibleUrl-short{color:#000000}.gs-webResult div.gs-visibleUrl-short{display:none}.gs-webResult div.gs-visibleUrl-long{display:block}.gs-promotion div.gs-visibleUrl-short{display:block}.gs-promotion div.gs-visibleUrl-long{display:none}.gsc-cursor-box{border-color:#000000}.gsc-results .gsc-cursor-box .gsc-cursor-page{border-color:#000000;background-color:#444444;color:#999999}.gsc-results .gsc-cursor-box .gsc-cursor-current-page{border-color:#000000;background-color:#000000;color:#000000}.gsc-webResult.gsc-result.gsc-promotion{border-color:#000000;background-color:#EEEEEE}.gsc-completion-title{color:#999999}.gsc-completion-snippet{color:#CCCCCC}.gs-promotion a.gs-title:link,.gs-promotion a.gs-title:link *,.gs-promotion .gs-snippet a:link{color:#0000CC}.gs-promotion a.gs-title:visited,.gs-promotion a.gs-title:visited *,.gs-promotion .gs-snippet a:visited{color:#0000CC}.gs-promotion a.gs-title:hover,.gs-promotion a.gs-title:hover *,.gs-promotion .gs-snippet a:hover{color:#444444}.gs-promotion a.gs-title:active,.gs-promotion a.gs-title:active *,.gs-promotion .gs-snippet a:active{color:#00CC00}.gs-promotion .gs-snippet,.gs-promotion .gs-title .gs-promotion-title-right,.gs-promotion .gs-title .gs-promotion-title-right *{color:#444444}.gs-promotion .gs-visibleUrl,.gs-promotion .gs-visibleUrl-short{color:#666666}",
+  "loaderPath": "jsapi",
+  "cse_token": "ABPF6Hj8WwB9h7nniQ6-RZ6QM5p93PVCFQ:1524267411783",
+  "isHostedPage": true
+});
+
+/*
+     FILE ARCHIVED ON 00:32:58 Jun 18, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:19:34 Jul 26, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 121.569 (3)
+  esindex: 0.007
+  captures_list: 136.769
+  CDXLines.iter: 11.756 (3)
+  PetaboxLoader3.datanode: 129.76 (4)
+  exclusion.robots: 0.144
+  exclusion.robots.policy: 0.133
+  RedisCDXSource: 0.53
+  PetaboxLoader3.resolve: 28.019
+  load_resource: 50.546
+*/
